b3b01e67780207784a638df38cd1a723aafba6f4
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-09-12 01:05:01 +0330

    added tree

---
+++ qtree.h
@@ -0,0 +1,70 @@
+#ifndef Q_TREE_H_
+#define Q_TREE_H_
+
+#include <glib-2.0/glib.h>
+
+typedef GTree		*qTree;
+typedef GTreeNode	*qTreeNode;
+typedef GList		*qTList;
+typedef GCompareFunc	qTCmpFn;
+typedef gbool		qTBool;
+typedef gpointer	qTPointer;
+typedef gconstpointer	qTConstP;
+
+
+static inline qTree
+new_qtree(qTCmpFn cmpfn)
+{
+	return (qTree)g_tree_new(cmpfn);
+}
+
+static inline void
+free_qtree(qTree tree)
+{
+	g_tree_destroy(tree);
+}
+
+static inline qTreeNode
+insert_into_tree(qTree tree, void *key, void *value)
+{
+	return (qTreeNode)g_tree_insert_node(tree, (qTConstP)key, (qTConstP)value);
+}
+
+static inline qTBool
+remove_from_tree(qTree tree, void *key)
+{
+	return (qTBool)g_tree_remove(tree, (qTConstP)key);
+}
+
+static inline qTreeNode
+get_first_node(qTree tree)
+{
+	return (qTreeNode)g_node_first(tree);
+}
+
+static inline qTreeNode
+get_last_node(qTree tree)
+{
+	return (qTreeNode)g_node_last(tree);
+}
+
+static inline qTreeNode
+get_next__node(qTree tree)
+{
+	return (qTreeNode)g_node_next(tree);
+}
+
+static inline qTreeNode
+get_prev_node(qTree tree)
+{
+	return (qTreeNode)g_node_previous(tree);
+}
+
+static inline qTPointer
+lookup_in_tree(qTree tree, void *key)
+{
+	return (qTPointer)g_tree_lookup(tree, (qTConstP)key);
+}
+
+
+#endif
